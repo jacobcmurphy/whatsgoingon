050eb2b165b0981ee56ea9ed60675e46da0d1be6
Sharat Sridhar <sharat.sridhar@gmail.com>
2014-03-06 14:39:35 -0500

    asset compile

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Authdemo::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  #Compiile Bootstrap
+  config.assets.compile = true
 end
