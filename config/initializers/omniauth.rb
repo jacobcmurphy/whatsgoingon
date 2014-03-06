89d773e6a24d5bf82ad9122289f5588c2818ba35
Jacob Murphy <murphorum@gmail.com>
2014-03-06 14:14:54 -0500

    fresh

--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '826643820685975', 'a836718c60a360b5c52ef2cf0b5f4fca', :scope => 'email, user_relationships, user_status,user_about_me publish_actions',    :display => 'popup'
-end
