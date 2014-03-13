5c5f6cbe0a44bcbc2a5fb61e04aa6c1116b8c53b
Samir Undavia <samir1@brandeis.edu>
2014-03-13 15:20:26 -0400

    added working markers

--- app/models/check_in.rb
+++ app/models/check_in.rb
@@ -1,8 +1,10 @@
 class CheckIn < ActiveRecord::Base
 	belongs_to :user
 
-	def self.most_recent
-    self.find(:all,
-              :conditions => ['check_ins.time > ?', 10.minutes.ago])
+	def self.most_recent uid
+	if uid.is_a? Integer
+	    self.select("distinct on (user_id) user_id, latitude, longitude")
+	    .where('check_ins.time > ? AND check_ins.user_id != ?', 12.minutes.ago, uid)
+	end
   end
 end
