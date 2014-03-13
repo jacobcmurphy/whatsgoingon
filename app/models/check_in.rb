d2abc2d8ea557eec8e6354768daa805bc3fcb98e
Samir Undavia <samir1@brandeis.edu>
2014-03-13 15:21:29 -0400

    changed time to 30 minutes

--- app/models/check_in.rb
+++ app/models/check_in.rb
@@ -4,7 +4,7 @@ class CheckIn < ActiveRecord::Base
 	def self.most_recent uid
 	if uid.is_a? Integer
 	    self.select("distinct on (user_id) user_id, latitude, longitude")
-	    .where('check_ins.time > ? AND check_ins.user_id != ?', 12.minutes.ago, uid)
+	    .where('check_ins.time > ? AND check_ins.user_id != ?', 30.minutes.ago, uid)
 	end
   end
 end
