7cf22b0dfbea1d39429f2ce749f5212a5ab74c45
Jacob Murphy <murphorum@gmail.com>
2014-03-12 23:30:13 -0400

    find recent

--- app/controllers/check_in_controller.rb
+++ app/controllers/check_in_controller.rb
@@ -1,15 +1,15 @@
 class CheckInController < ApplicationController
   def create
-  	if(current_user)
-	    @check = CheckIn.new(check_in_params, time: Time.now, user_id: current_user.id)
-	    @check.save
-	end
-
-	# redirect_to root_url
+    if(current_user)
+      @check = CheckIn.new(check_in_params)
+      @check.time = Time.now
+      @check.user_id = current_user.id
+      @check.save
   end
+ end
 
   private 
   def check_in_params
-     params.permit(:user_id, :latitude, :longitude)
+    params.permit(:user_id, :latitude, :longitude, :user_id, :time)
   end
 end
--- app/models/check_in.rb
+++ app/models/check_in.rb
@@ -1,3 +1,8 @@
 class CheckIn < ActiveRecord::Base
 	belongs_to :user
+
+	def self.most_recent
+    self.find(:all,
+              :conditions => ['check_ins.time > ?', 10.minutes.ago]).uniq.pluck(:user_id)
+  end
 end
