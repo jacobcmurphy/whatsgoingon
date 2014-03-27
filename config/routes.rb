04d4bdb7959ac5226d0368af4fe2ac6fac46ca1b
Samir Undavia <samir1@brandeis.edu>
2014-03-26 22:33:19 -0400

    edit account button

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Whatsgoingon::Application.routes.draw do
 
+  get "marker_requests/newRequest"
   devise_for :admin_users, ActiveAdmin::Devise.config
 
   match "groups" => "groups#index", via: :get
