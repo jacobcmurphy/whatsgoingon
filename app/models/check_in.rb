5d03ba121bc2088e6fb46e19290490237aa83e52
Jacob Murphy <murphorum@gmail.com>
2014-04-02 21:21:20 -0400

    check in modified

--- app/models/check_in.rb
+++ app/models/check_in.rb
@@ -3,9 +3,6 @@ class CheckIn < ActiveRecord::Base
 
 	def self.most_recent uid
 	if uid.is_a? Integer
-	    #self.select("distinct on (user_id) user_id, latitude, longitude")
-	    #.where('check_ins.time > ? AND check_ins.user_id != ?', 15.minutes.ago, uid)
-
 	    CheckIn.find_by_sql([
 	    	"SELECT distinct ON (c.user_id) c.user_id, c.latitude, c.longitude, c.location
 	    	FROM check_ins AS c
