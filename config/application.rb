e27d67f199b62d5730d75f15880c6ccc9a651578
Jacob Murphy <murphorum@gmail.com>
2014-04-03 21:49:14 -0400

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -21,16 +21,11 @@ module Whatsgoingon
     # config.i18n.default_locale = :de
 
     social_keys = File.join(Rails.root, 'config', 'social_keys.yml')
-    config = HashWithIndifferentAccess.new(YAML::load(IO.read(social_keys)))[Rails.env]
-    config.each do |k,v|
-      ENV[k.upcase] ||= v
-    end
-=begin
+
     CONFIG = HashWithIndifferentAccess.new(YAML::load(IO.read(social_keys)))[Rails.env]
     CONFIG.each do |k,v|
       ENV[k.upcase] ||= v
     end
-=end
 
   end
 end
