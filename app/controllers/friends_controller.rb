21fab791f57a5398ca5cf0dbaae3f54d73591104
Samir <samir1@brandeis.edu>
2014-04-10 19:37:38 -0400

    something david sent me with my previous change

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,72 +1,73 @@
 class FriendsController < ApplicationController
-  def new
-  	@friend = Friend.new
-  end
+    def new
+      @friend = Friend.new
+    end
 
-  def create
-    if(user_signed_in?)
-      current_user.friends.create(user_id: current_user.id, accepted: false, friend_id: params[:friend_id])
-    end
-    render nothing: true
-   end
+    def create
+        if(user_signed_in?)
+            current_user.friends.create(user_id: current_user.id, accepted: false, friend_id: params[:friend_id])
+        end
+        render nothing: true
+     end
 
-  def destroy
-  	if user_signed_in?
-      Friend.find(params[:fid].to_i).destroy
-      redirect_to friend_path
-    else 
-      redirect_to root_url
-    end
-  end
+    def destroy
+      if user_signed_in?
+            Friend.find(params[:fid].to_i).destroy
+            redirect_to friend_path
+        else 
+            redirect_to root_url
+        end
+    end
 
-  def show
-    f_ids = []
-    
-    current_user.friends.each do |f|
-      f_ids << f.user_id
-    end
+    def show
+        f_ids = []
+        
+        current_user.friends.each do |f|
+            f_ids << f.friend_id
+        end
 
-    @friend = Friend.new
-    @friends = User.find(f_ids)
-  end
 
-  def reject
-  	if user_signed_in?
-  		current_user.friends.where(friend_id: friend_id).first.destroy!
-  	end
-  	render nothing: true
-  end
+        @friend = Friend.new
+        @friends = User.find(f_ids)
+    end
 
-  def accept
-  	if user_signed_in?
-  		current_user.friends.create(user_id: current_user.id, friend_id: friend_id, accepted: true)
-  		accepted_friend = Friend.where(user_id: friend_id, friend_id: current_user.id).first
-      accepted_friend.accepted = true
-      accepted_friend.save!
-  	end
-  	render nothing: true
-  end
+    def reject
+      if user_signed_in?
+        current_user.friends.where(friend_id: friend_id).first.destroy!
+      end
+      render nothing: true
+    end
 
-  def getmarkers
-  	cuid = current_user.id
-  	dbMarkers = {}
+    def accept
+      if user_signed_in?
+        current_user.friends.create(user_id: current_user.id, friend_id: friend_id, accepted: true)
+        accepted_friend = Friend.where(user_id: friend_id, friend_id: current_user.id).first
+            accepted_friend.accepted = true
+            accepted_friend.save!
+      end
+      render nothing: true
+    end
 
-  	user_friends = Friend.most_recent(cuid)
+    def getmarkers
+      cuid = current_user.id
+      dbMarkers = {}
 
-  	for i in 0...user_friends.length
-  		thisMarker = { user_friends[i][:user_id] => [
-	                  { name: user_friends[i][:name] },
-	                  { lat:  user_friends[i][:latitude] },
-	                  { lng:  user_friends[i][:longitude] },
-                    { color_status:  user_friends[i][:color_status] }
-	                ]
-	            }
-	    dbMarkers.merge! thisMarker
-  	end
+      user_friends = Friend.most_recent(cuid)
 
-  	respond_to do |format|
-  		format.json { render json: dbMarkers }
-    end
-  end
+      for i in 0...user_friends.length
+        thisMarker = { user_friends[i][:user_id] => [
+                          { name: user_friends[i][:name] },
+                          { lat:  user_friends[i][:latitude] },
+                          { lng:  user_friends[i][:longitude] },
+                          { color_status:  user_friends[i][:color_status] }
+                      ]
+                }
+        dbMarkers.merge! thisMarker
+    end
+
+    respond_to do |format|
+      format.json { render json: dbMarkers }
+      end
+  end
 
 end
