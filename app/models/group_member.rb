9b3ffe567365f3d14d952823c802a684fbee5b17
David Giliotti <davidgil@brandeis.edu>
2014-04-29 13:45:39 -0400

    Got rid of regroup_member

--- app/models/group_member.rb
+++ app/models/group_member.rb
@@ -1,17 +1,4 @@
 class GroupMember < ActiveRecord::Base
 	belongs_to :group
 
-	def self.notifications uid
-		#if uid is_a? Integer
-			notifs = self.where(friend_id: uid, accepted: false)
-			note_names = []
-
-			notifs.each do |note|
-				friend = User.find(note.user_id)
-				note_names <<  [friend.id, friend.name]
-			end
-
-			note_names
-		#end
-	end
 end
