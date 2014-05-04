2548def936ecab94083b0d6a077dfbf8b9316f7f
David Giliotti <davidgil@brandeis.edu>
2014-05-03 22:03:54 -0400

    Events Controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,20 +1,13 @@
 class EventsController < ApplicationController
-  def index
-  	if user_signed_in?
-      @events = Event.find_events(current_user.id)
-    else
-      redirect_to root_url
-    end
-  end
 
-
-  def show
+  def create
     if user_signed_in?
-      @events = Event.where(id: params[:id].to_i)
-      group = Group.find(@events.first.group_id)
-      if group.id = current_user.id # the user can edit this event
-      else
-        redirect_to events_path
+      group_ids = params[:group_ids].map(&:to_i)
+      groups = Group.find(group_ids)
+
+      groups.each do |g|
+          g_event = g.events.create(title: params[:title], description: params[:description], latitude: params[:lat], longitude: params[:long], group_id: g.id.to_i)
+          g_event.save!
       end
     else
       redirect_to root_url
@@ -22,6 +15,24 @@ class EventsController < ApplicationController
   end
 
 
-  def destroy
+  def show
+    @events = Event.find_by_sql("
+        SELECT * 
+        FROM events AS e
+        WHERE e.group_id IN 
+           (SELECT g.id
+                FROM group_members AS gm
+                WHERE e.group_id = gm.id AND (gm.user_id = ? OR gm.friend_id = ?))", current_user.id, current_user.id)
   end
+
+
+  def destroy
+    if user_signed_in?
+      Event.find(params[:event_id].to_i).destroy
+      redirect_to events_path
+    else 
+      redirect_to root_url
+    end
+  end
+
 end
