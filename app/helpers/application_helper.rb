3aa354a9c247160dc170d0fef16f41fcf87e7eaa
Samir <samir1@brandeis.edu>
2014-05-05 00:34:01 -0400

    dry

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,9 @@
 module ApplicationHelper
 	def buildings 
-    	["SSC", "Library", "Usdan", "Usen", "Renfield", "Deroy", "Shapiro", "Rosie North", "Rosie East", "Rosie South", "SCC", "Sherman", "Ridgewood A", "Ridgewood B", "Ridgewood C", "Reitman", "Cable", "Gordon", "Scheffres", "Hass", "Pom", "ICC", "Rabb Building", "Olin-Sang", "Golding", "Lown", "Shiffman", "Mandel", "Heller", "ASAC", "Castle", "Brown", "Schwartz", "Ziv 127", "Ziv 128", "Ziv 129", "Ziv 130", "Admissions", "Slosberg", "Gosman", "Medical Center", "Public Safety", "Pearlman Hall", "Mods", "Faculty Club", "Rose Art Museum", "Pollack", "Fine Arts Studio", "IBS", "Theater", "Village A", "Village B", "Village C", "Vertica", "Volen", "Goldsmith", "Bassine", "Gzang", "Abelson-Bass", "Rosenstiel", ""]      
-  	end
-
-  	def statusNames 
-  		["In Class", "Working", "Studying", "Eating", "Partying", "Relaxing", "In Class", "Working", "Studying", "Eating", "Partying", "Relaxing", "Is"]
+		["SSC", "Library", "Usdan", "Usen", "Renfield", "Deroy", "Shapiro", "Rosie North", "Rosie East", "Rosie South", "SCC", "Sherman", "Ridgewood A", "Ridgewood B", "Ridgewood C", "Reitman", "Cable", "Gordon", "Scheffres", "Hass", "Pom", "ICC", "Rabb Building", "Olin-Sang", "Golding", "Lown", "Shiffman", "Mandel", "Heller", "ASAC", "Castle", "Brown", "Schwartz", "Ziv 127", "Ziv 128", "Ziv 129", "Ziv 130", "Admissions", "Slosberg", "Gosman", "Medical Center", "Public Safety", "Pearlman Hall", "Mods", "Faculty Club", "Rose Art Museum", "Pollack", "Fine Arts Studio", "IBS", "Theater", "Village A", "Village B", "Village C", "Vertica", "Volen", "Goldsmith", "Bassine", "Gzang", "Abelson-Bass", "Rosenstiel", ""]
 	end
 
-	def inbuildings
-		["in the SCC", "in the Library", "in Usdan", "in Usen", "in Renfield", "in Deroy", "in Shapiro", "in Rosie North", "in Rosie East", "in Rosie South", "in the SCC", "in Sherman", "in Ridgewood A", "in Ridgewood B", "in Ridgewood C", "in Reitman", "in Cable", "in Gordon", "in Scheffres", "in Hass", "in Pom", "in the ICC", "in  the Rabb Building", "in Olin-Sang", "in Golding", "in Lown", "in Shiffman", "in Mandel", "in Heller", "in ASAC", "in the Castle", "in Brown", "in Schwartz", "in Ziv 127", "in Ziv 128", "in Ziv 129", "in Ziv 130", "in Admissions", "in Slosberg", "in Gosman", "in Medical Center", "at Public Safety", "in Pearlman Hall", "at the Mods", "in the Faculty Club", "in the Rose Art Museum", "in Pollack", "in the Fine Arts Studio", "in IBS", "in the Theater", "in Village A", "in Village B", "in Village C", "at Vertica", "in Volen", "in Goldsmith", "in Bassine", "in Gzang", "in Abelson-Bass", "in Rosenstiel", ""]
-  	end
-
+	def statusNames
+		["In Class", "Working", "Studying", "Eating", "Partying", "Relaxing", "In Class", "Working", "Studying", "Eating", "Partying", "Relaxing", "Is"]
+	end
 end
