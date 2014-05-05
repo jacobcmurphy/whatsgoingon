80b7e3c742ff004123afd12f52b5d619e4d4e0c8
David Giliotti <davidgil@brandeis.edu>
2014-05-05 13:23:28 -0400

    Fixed Consistency

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
 	def buildings 
-		["SSC", "Library", "Usdan", "Usen", "Renfield", "Deroy", "Shapiro", "Rosie North", "Rosie East", "Rosie South", "SCC", "Sherman", "Ridgewood A", "Ridgewood B", "Ridgewood C", "Reitman", "Cable", "Gordon", "Scheffres", "Hass", "Pom", "ICC", "Rabb Building", "Olin-Sang", "Golding", "Lown", "Shiffman", "Mandel", "Heller", "ASAC", "Castle", "Brown", "Schwartz", "Ziv 127", "Ziv 128", "Ziv 129", "Ziv 130", "Admissions", "Slosberg", "Gosman", "Medical Center", "Public Safety", "Pearlman Hall", "Mods", "Faculty Club", "Rose Art Museum", "Pollack", "Fine Arts Studio", "IBS", "Theater", "Village A", "Village B", "Village C", "Vertica", "Volen", "Goldsmith", "Bassine", "Gzang", "Abelson-Bass", "Rosenstiel", ""]
+		["SSC", "Library", "Usdan", "Usen", "Renfield", "Deroy", "Shapiro", "Rosie North", "Rosie East", "Rosie South", "SCC", "Sherman", "Ridgewood A", "Ridgewood B", "Ridgewood C", "Reitman", "Cable", "Gordon", "Scheffres", "Hass", "Pom", "ICC", "Rabb Building", "Olin-Sang", "Golding", "Lown", "Shiffman", "Mandel", "Heller", "ASAC", "Castle", "Brown", "Schwartz", "Ziv 127", "Ziv 128", "Ziv 129", "Ziv 130", "Admissions", "Slosberg", "Gosman", "Medical Center", "Public Safety", "Pearlman Hall", "Mods", "Faculty Club", "Rose Art Museum", "Pollack", "Fine Arts Studio", "IBS", "Theater", "Village A", "Village B", "Village C", "Vertica", "Volen", "Goldsmith", "Bassine", "Gzang", "Abelson-Bass", "Rosenstiel", "Not In a Building"]
 	end
 
 	def statusNames
-		["In Class", "Working", "Studying", "Eating", "Partying", "Relaxing", "In Class", "Working", "Studying", "Eating", "Partying", "Relaxing", "Is"]
+		["Is In Class", "Is Working", "Is Studying", "Is Eating", "Is Partying", "Is Relaxing", "Is In Class", "Is Working", "Is Studying", "Is Eating", "Is Partying", "Is Relaxing", "Is"]
 	end
 end
